b14b572dc1506993aace7926263b61d6242d407f
Lagolas <assass@aliyun.com>
2015-06-21 19:19:22 +0800

    target config

--- Application/Common/Controller/InitController.class.php
+++ Application/Common/Controller/InitController.class.php
@@ -10,5 +10,12 @@ class InitController extends Controller{
         define('SOURCE_PATH', COMMON_PATH.'View/statics/');//静态资源目录
         define('PUBLIC_PATH', "/Public/");
         define('UPLOAD_ROOT_PATH','./Public/');
+        $this->targetDB = 'mysql://root:admin@localhost:3306/pctest';
+        $this->targetPre = "pc_";
+        $this->targetNews = "news";
+        $this->targetNewsData = "news_data";
+        $this->targetHost = "http://pctest.cc";
+        $this->targetSiteName = "<a href='http://guaguayule.com'>瓜瓜娱乐</a>";
+        $this->targetSiteNameNoa = "瓜瓜娱乐";
     }
 }
