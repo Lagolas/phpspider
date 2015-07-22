115f38ad2e16c8f5b824b8dfcba548f624c13e2c
Lagolas <assass@aliyun.com>
2015-07-22 20:37:54 +0800

    DEFAULT_CONTROLLER

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -2,6 +2,7 @@
 return array(
 	//'配置项'=>'配置值'
     'URL_MODEL' => 0,
+    'DEFAULT_CONTROLLER'=> 'crawl',
     'DB_TYPE'           => 'mysql', // 数据库类型
     'DB_HOST'           => 'localhost', // 服务器地址
     'DB_NAME'           => 'spider', // 数据库名
